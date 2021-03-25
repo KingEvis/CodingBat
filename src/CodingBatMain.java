a28751d376d86e06f6ade3ebdaf19f3ff78fac49
KingEvis <evaldas.moisenka@gmail.com>
2021-03-25 20:25:27 +0200

    Added solution in3050

--- src/CodingBatMain.java
+++ src/CodingBatMain.java
@@ -133,4 +133,7 @@ public class CodingBatMain {
         }
         return -1;
     }
+    public boolean in3050(int a, int b) {
+        return (a>=40&&a<=50&&b>=40&&b<=50)||(a>=30&&a<=40&&b>=30&&b<=40);
+    }
 }
